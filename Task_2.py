758e1860fb88c36864ec6fc69176b67b290a2192
onyekachi <onyekachi.ogbue@gmail.com>
2025-05-16 22:10:04 +0100

    first commit

--- Task_2.py
+++ Task_2.py
@@ -12,3 +12,12 @@ print(6 // 4)
 print(14 % 4)
 print(12 % 4.5)
 print(-4 + 4)
+var=1
+account_balance= 1000
+house_number= '45b'
+job_position= 'network development engineer'
+print(var, account_balance, house_number, job_position )
+var = 100
+
+var = var+200 + 300
+print(var)
